095483aa727da52c7211ab3d93542014fb99f18a
Andre Felicio <felicioandre@LAPTOP-OP02RB1E.localdomain>
2021-10-18 19:32:49 -0300

    feat(App.js): add handleAddRepository and handleRemoveRepository logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+  const [repositories, setRepositories] = useState([]);
 
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const newRepository = {
+      title: `Novo Repositório ${Date.now()}`,
+      url: `https://github.com/rocketseat-education/bootcamp-gostack-desafios/`,
+      techs: [`Javascript`, `CSS`],
+    };
+    const response = await api.post("repositories", newRepository);
+
+    setRepositories([...repositories, response.data]);
+  }
   async function handleRemoveRepository(id) {
     // TODO
+    try {
+      await api.delete(`repositories/${id}`)
+      const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+      const newList = [...repositories];
+      newList.splice(repositoryIndex, 1);
+      setRepositories([...newList]);
+    } catch (e) {
+      console.error("Error when trying to delete Repository: ", id);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
